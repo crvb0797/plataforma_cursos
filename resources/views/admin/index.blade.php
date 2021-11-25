139a62d55e9fb2f6141e8f1fe0a44e58d3779b39
Carlos Villatoro <crvb0797@gmail.com>
2021-11-25 17:49:06 -0600

    Agregamos Admin LTE para la parte administradora
    Integramos la plantilla de AdminLTE para el administrador y copiamos su estructura para la vista index admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,21 @@
+@extends('adminlte::page')
 
+@section('title', 'Villatoro Dev')
+
+@section('content_header')
+    <h1>Villatoro.dev</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido administrador</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        console.log('Hi!');
+    </script>
+@stop
