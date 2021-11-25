ed9093d0d86481ff7e8f0df686cc841b40d060d8
Carlos Villatoro <crvb0797@gmail.com>
2021-11-25 13:14:49 -0600

    Cremos las migraciones para el contenido del curso
    * Requerimientos
    * Metas
    * Audiencia
    * Secciones
    
    Se ejecutaron las migraciones y ya se cuenta con las llaves foraneas para la tabla de cursos

---
+++ database/migrations/2021_11_25_190920_create_requirements_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequirementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requirements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+
+            /* LLAVES FORANEAS */
+            $table->unsignedBigInteger('course_id');
+
+            /* RESTRICCIONES DE LLAVES FORANEAS */
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requirements');
+    }
+}
