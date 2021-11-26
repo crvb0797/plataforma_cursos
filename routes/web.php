ee1369fea01f1d49af7984802ac68ca3950a5f2e
Carlos Villatoro <crvb0797@gmail.com>
2021-11-26 09:21:24 -0600

    Creamos un controlador para el home de la aplicación
    En este controlador solo invocamos a un unico método al cual le llamaremos __invoke esto por que no se hara cargo de nada más que de mostrar la vista welome

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', HomeController::class)->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
